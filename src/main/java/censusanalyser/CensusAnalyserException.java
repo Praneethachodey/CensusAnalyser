30c1dd6669d60395772964e1619166ca1271e4f4
Praneethachodey <praneethachodey@gmail.com>
2020-10-20 02:46:13 +0530

    [Praneetha]Edited: made changes to exception file

--- src/main/java/censusanalyser/CensusAnalyserException.java
+++ src/main/java/censusanalyser/CensusAnalyserException.java
@@ -3,7 +3,7 @@ package censusanalyser;
 public class CensusAnalyserException extends Exception {
 
     enum ExceptionType {
-        CENSUS_FILE_PROBLEM, UNABLE_TO_PARSE , CENSUS_FILE_TYPE_PROBLEM
+        CENSUS_FILE_PROBLEM, UNABLE_TO_PARSE , STATE_FILE_PROBLM
     }
 
     ExceptionType type;
